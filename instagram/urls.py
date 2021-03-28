aee79fa9b4341c82da14e8e4437716ba5262adc7
nicholas-oduor <oduor5742@gmail.com>
2021-03-28 11:13:51 +0300

    inserted url patterns

---
+++ instagram/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+urlpatterns =[
+    path('search/', views.search_results, name='search_results'),
+    path('new/comment/', views.new_comment, name='new_comment'),
+    path('new/post/', views.new_post, name='new_post'),
+    path('accounts/profile/', views.new_profile, name='new_profile'),
+    path('image/(\d+)', views.get_image, name='image_results'),
+    path('like/<int:pk>/', views.like_image, name='like_post'),
+    
+]
