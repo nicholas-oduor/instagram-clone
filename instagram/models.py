2a22f6b37d723327eb70dd8cf05e06d2f0aa311b
nicholas-oduor <oduor5742@gmail.com>
2021-03-28 11:00:28 +0300

    created class Image methods

--- instagram/models.py
+++ instagram/models.py
@@ -31,3 +31,46 @@ class Profile(models.Model):
         
     def __str__(self):
         return self.bio 
+
+class Image(models.Model):
+    image = CloudinaryField('image')
+    image_name = models.CharField(max_length=255)
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True)
+    likes = models.ManyToManyField(User, related_name = 'likes', blank = True)
+    
+    
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete()
+        
+    def total_likes(self):
+        return self.likes.count()
+        
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+    
+    @classmethod
+    def get_image(request, id):
+        try:
+            image = Image.objects.get(pk = id)
+            print(image)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return image
+    
+    @classmethod
+    def search_by_author(cls, Author):
+        images = cls.objects.filter(Author__user__icontains=Author)
+        return images
+
+  
+    def __str__(self):
+        return self.image_name 
