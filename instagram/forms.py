343b31b5635a437d7f68e1404fd99f32a438e0ff
nicholas-oduor <oduor5742@gmail.com>
2021-03-28 10:44:40 +0300

    inserted class NewsLetterForm

--- instagram/forms.py
+++ instagram/forms.py
@@ -12,3 +12,7 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
