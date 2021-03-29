1a0b44e4b51d4d369affedb82673939df460acbc
nicholas-oduor <oduor5742@gmail.com>
2021-03-29 11:27:51 +0300

    added cloudinary.config api,name & secret

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+cloudinary.config( 
+  cloud_name = "dec8li4lj", 
+  api_key = "137358617789222", 
+  api_secret = "-ZrBTOGfZcirrxMN1oJXw1jJ2cg" 
+)
