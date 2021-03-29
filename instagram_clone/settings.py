2c943e6d66c49136a0f22c45cf428fe2f9b10613
nicholas-oduor <oduor5742@gmail.com>
2021-03-29 11:32:07 +0300

    imported cloudinary files

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+
+from pathlib import Path
 import os
+from decouple import config,Csv
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
